6e5d54c2501acfa3b09b362e0d405d3bee18f831
Mr-Ndi <onlythenotes@gmail.com>
2024-12-13 15:42:27 +0200

    testing the effecting of dependencies

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,25 @@
 // import  Message  from './Message';
-import ListGroup from "./components/ListGroup";
 
-export default function App() {
+import { useEffect, useRef } from "react";
+
+// import ListGroup from "./components/ListGroup";
+const connect = ()=> console.log('Connecting')
+const diconnect = ()=> console.log('Disconnecting')
+
+function App() {
+  const ref = useRef<HTMLInputElement>(null)
+  useEffect(()=>{
+    if (ref.current) ref.current.focus();
+  })
+
+  useEffect(()=> {
+    document.title = 'Nje naa Mbaduko'
+  })
   return (
     <div>
-      <ListGroup />
+      <input type="text" className="form-control" />
     </div>
   );
 }
+
+export default App
