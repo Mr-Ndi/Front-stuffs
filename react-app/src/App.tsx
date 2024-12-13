4b4173b22707d1dec69971b19c466891b62c66dc
Mr-Ndi <onlythenotes@gmail.com>
2024-12-13 16:02:01 +0200

    before fetching from db

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,25 +1,13 @@
-// import  Message  from './Message';
-
 import { useEffect, useRef } from "react";
 
-// import ListGroup from "./components/ListGroup";
-const connect = ()=> console.log('Connecting')
-const diconnect = ()=> console.log('Disconnecting')
+const connect = ()=> console.log('Connecting ....');
+const disconnect = ()=> console.log('Disconnecting ....');
 
 function App() {
-  const ref = useRef<HTMLInputElement>(null)
   useEffect(()=>{
-    if (ref.current) ref.current.focus();
-  })
-
-  useEffect(()=> {
-    document.title = 'Nje naa Mbaduko'
-  })
-  return (
-    <div>
-      <input type="text" className="form-control" />
-    </div>
-  );
+    connect();
+    return ()=> disconnect()
+  });
 }
 
 export default App
