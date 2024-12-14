6337952bf28febc335d60c7314c9d65c953e2e91
Mr-Ndi <onlythenotes@gmail.com>
2024-12-14 07:41:12 +0200

    fetching from fake backend JSONPLACEHOLDER

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,21 @@
-import { useEffect, useRef } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const connect = ()=> console.log('Connecting ....');
-const disconnect = ()=> console.log('Disconnecting ....');
-
+interface User{
+  id: number;
+  name: string
+}
 function App() {
-  useEffect(()=>{
-    connect();
-    return ()=> disconnect()
-  });
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then(res => setUsers(res.data))
+  }, [])
+  return <ul>
+    {users.map(user => <li key={user.id}>{user.name}</li>)}
+  </ul>; 
 }
 
-export default App
+export default App;
+ 
