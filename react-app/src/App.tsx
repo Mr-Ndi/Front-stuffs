fb8c9e01da02fe52ff74e73a5f347997e15ada20
Mr-Ndi <onlythenotes@gmail.com>
2024-12-14 08:23:27 +0200

    Building forms

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,31 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import Form from './components/Form/Forms';
+// import { useEffect, useState } from 'react';
 
-interface User{
-  id: number;
-  name: string
-}
+// interface User{
+//   id: number;
+//   name: string
+// }
+// function App() {
+//   const [users, setUsers] = useState<User[]>([]);
+
+//   useEffect(() => {
+//     axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+//       .then(res => setUsers(res.data))
+//   }, [])
+//   return <ul>
+//     {users.map(user => <li key={user.id}>{user.name}</li>)}
+//   </ul>; 
+// }
+
+// export default App;
+
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
 
-  useEffect(() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then(res => setUsers(res.data))
-  }, [])
-  return <ul>
-    {users.map(user => <li key={user.id}>{user.name}</li>)}
-  </ul>; 
-}
-
-export default App;
- 
+export default Form
