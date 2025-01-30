6d8cfc5c59ef45aafe9c5ab482594181e27197c1
Mr-Ndi <onlythenotes@gmail.com>
2025-01-30 10:16:35 +0200

    Global Styles

---
+++ first-app/app/components/AddToCard.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <button onClick={ ()=> { console.log('clicked')}}>Add to Cart</button>
+  )
+}
+
+export default AddToCard
---
+++ first-app/app/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddToCard from './AddToCard'
+
+function ProductCard() {
+  return (
+    <AddToCard />
+  )
+}
+
+export default ProductCard
